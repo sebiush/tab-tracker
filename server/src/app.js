1e5a7bee5a49819c2fb0e6a03e61de696ef7616a
sebiush <sebiush@gmail.com>
2018-10-02 21:44:11 +0200

    update

--- server/src/app.js
+++ server/src/app.js
@@ -2,17 +2,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const { sequelize } = require('../models')
+const config = require('../config/config')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/register', (req, res) => {
-  res.send({
-    message: `Hello ${req.body.email} Your user was registered`
-  })
-})
+require('../routes')(app)
 
-app.listen(process.env.PORT || 8082)
-console.log('listening on port 8082')
+sequelize.sync({ force: false })
+    .then(() => {
+        app.listen(config.port)
+        console.log(`listening on port ${config.port}`)
+    })
+
+
