5aaa2f48f34cd05b8b1d2e77010a217574f1d2d6
sebiush <sebiush@gmail.com>
2018-10-02 19:55:13 +0200

    setting up

--- server/src/app.js
+++ server/src/app.js
@@ -8,10 +8,10 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
-    res.send({
-    message: "hello world"
-    })
+app.post('/register', (req, res) => {
+  res.send({
+    message: `Hello ${req.body.email} Your user was registered`
+  })
 })
 
 app.listen(process.env.PORT || 8082)
